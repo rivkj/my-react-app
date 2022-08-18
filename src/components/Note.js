3ad3a159aa06b2925ecb2a63faf6692d9778b7ef
Kimberly Rivas <rivaskj07@gmail.com>
2022-08-18 15:39:25 -0400

    updated notes app

---
+++ src/components/Note.js
@@ -0,0 +1,25 @@
+import React, {Component, createRef} from 'react';
+import '../css/Note.css';
+import PropTypes from 'prop-types';
+
+class Note extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="col-sm-6">
+        <div className="card card-view">
+          <div className="card-body">
+            <h5 className="card-title">{this.props.title}</h5>
+            <p>This is a very long note that we just made up</p>
+            <button className="btn btn-info">Edit</button>
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
